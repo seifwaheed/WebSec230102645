9d10b2fb4bea3ed83f01f8b822d691f24addcb91
Seif Waheed <135646856+seifwaheed@users.noreply.github.com>
2025-02-22 15:57:57 +0200

    a

--- seif230102645/resources/views/prime.blade.php
+++ seif230102645/resources/views/prime.blade.php
@@ -1,41 +1,24 @@
-<?php
-
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-
-class PrimeNumberController extends Controller
-{
-  
-    public function isPrime(Request $request)
-    {
-        $number = $request->input('number');
-        
-        if (!is_numeric($number) || $number < 2) {
-            return response()->json(['error' => 'Please enter a valid positive integer greater than 1.']);
-        }
-        
-        if ($this->checkPrime($number)) {
-            return response()->json(['result' => "$number is a prime number."]);
-        }
-        
-        return response()->json(['result' => "$number is not a prime number."]);
-    }
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<div class='card m-3'>
+    <div class='card-header'>Prime Numbers</div>
+    <div class="card=body">
+        @foreach (range(1, 100) as i)
+    </div>
+</div>
 
 
-    private function checkPrime($number)
-    {
-        if ($number <= 1) return false;
-        if ($number <= 3) return true;
-        
-        if ($number % 2 == 0 || $number % 3 == 0) return false;
-        
-        for ($i = 5; $i * $i <= $number; $i += 6) {
-            if ($number % $i == 0 || $number % ($i + 2) == 0) {
-                return false;
-            }
-        }
-        
-        return true;
-    }
-}
+
+
+
+
+    
+</body>
+</html>

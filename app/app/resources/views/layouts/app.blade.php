9676472ca10780623ab50458c93221465c4e19f6
Seif Waheed <135646856+seifwaheed@users.noreply.github.com>
2025-03-21 10:35:30 +0200

    d

--- app/app/resources/views/layouts/app.blade.php
+++ app/app/resources/views/layouts/app.blade.php
@@ -36,12 +36,7 @@
                             </form>
                         </li>
                     @else
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">Login</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">Register</a>
-                        </li>
+                        
                     @endauth
                 </ul>
             </div>
--- app/app/routes/web.php
+++ app/app/routes/web.php
@@ -22,21 +22,3 @@ Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.upda
 
 Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
 
-Route::get('/login', [AuthController::class, 'loginForm'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
-
-Route::get('/register', [AuthController::class, 'registerForm'])->name('register');
-Route::post('/register', [AuthController::class, 'register']);
-
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
-// Protect book routes (Only Authenticated Users)
-Route::middleware(['auth'])->group(function () {
-    Route::get('/books', [BookController::class, 'index'])->name('books.index');
-    Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
-    Route::post('/books', [BookController::class, 'store'])->name('books.store');
-    Route::get('/books/{book}', [BookController::class, 'show'])->name('books.show');
-    Route::get('/books/{book}/edit', [BookController::class, 'edit'])->name('books.edit');
-    Route::put('/books/{book}', [BookController::class, 'update'])->name('books.update');
-    Route::delete('/books/{book}', [BookController::class, 'destroy'])->name('books.destroy');
-});
